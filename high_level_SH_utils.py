e26b854827a0dfd00d735300f51a093228ec9b58
Kevin Gilliam <keving@arizona.edu>
2025-05-30 12:26:30 -0700

    Updates from onsky tests

--- high_level_SH_utils.py
+++ high_level_SH_utils.py
@@ -67,9 +67,9 @@ def xyr_pupil_definition(reference_path, save_path, redefine_pupil = False):
 
     return xyr, jup_image
 
-def lenslet_definition(folder_path, reference_path, xyr, output_plots, recompute_rotation = False):
-    if os.path.isfile(reference_path + 'references.pkl') and not recompute_rotation:
-        with open(reference_path + 'references.pkl', 'rb') as f:
+def lenslet_definition(reference_path, save_path, xyr, output_plots, recompute_rotation = False):
+    if os.path.isfile(save_path + 'references.pkl') and not recompute_rotation:
+        with open(save_path + 'references.pkl', 'rb') as f:
             loaded_dict = pickle.load(f)
         referenceX = loaded_dict["refX"]
         referenceY = loaded_dict["refY"]
@@ -77,13 +77,13 @@ def lenslet_definition(folder_path, reference_path, xyr, output_plots, recompute
         nominalSpot = loaded_dict["nominalSpot"]
         rotation = loaded_dict["rotation"]
     else:
-        referenceX, referenceY, magnification, nominalSpot, rotation = define_reference(folder_path, xyr, output_plots)
+        referenceX, referenceY, magnification, nominalSpot, rotation = define_reference(reference_path, xyr, output_plots)
         references = {"refX":referenceX,
                       "refY":referenceY,
                       "magnification":magnification,
                       "nominalSpot":nominalSpot,
                       "rotation":rotation}
-        with open(reference_path + 'references.pkl', 'wb') as f:
+        with open(save_path + 'references.pkl', 'wb') as f:
             pickle.dump(references,f)
     return referenceX,referenceY,magnification,nominalSpot,rotation
 
@@ -94,14 +94,14 @@ def process_folder_of_images(folder_path,rotation, xyr, referenceX, referenceY, 
     for num, filename in enumerate(os.listdir(folder_path)):
         starting_time = time.time()
         image,_ = prepare_image(folder_path + '/' + filename, rotation, xyr)
-        shape_diff = compute_wavefront(image,referenceX, referenceY, magnification, nominalSpot, xyr_cropped, output_plots=True)
+        shape_diff = compute_wavefront(image,referenceX, referenceY, magnification, nominalSpot, xyr_cropped, output_plots)
 
         #if num==0 or shape_diff.shape[0] != Z[1].shape[0]:
         #    Z = General_zernike_matrix(44,int(15*25.4 * 1e3),int(3*25.4*1e3),shape_diff.shape[0])
         #M,C = get_M_and_C(shape_diff, Z)
         #updated_surface = remove_modes(M,C,Z,remove_coef)*1e3
         updated_surface = shape_diff.copy()
-        
+        updated_surface = updated_surface * 1e3
         surfaces.append(updated_surface)
 
         if True:
@@ -129,14 +129,17 @@ def suggest_next_iteration_of_TEC_correction(current_eigenvalues, folder_path, t
 
     interpolated_surface = griddata_interpolater(mean_surface, eigenvectors[0], clear_aperture_outer,
                                                  clear_aperture_inner)
-    
+
+    if True:  #Fix this during a daytime hour so that you don't need to recompute every time
+        Z = General_zernike_matrix(44,int(15*25.4 * 1e3),int(3*25.4*1e3),500)
+
     M,C = get_M_and_C(interpolated_surface,Z)
     remove_coef = [0,1,2,4]
-    updated_surface = remove_modes(M,C,Z,remove_coef)*-1e3
+    updated_surface = remove_modes(M,C,Z,remove_coef)*-1
 
     reduced_surface, eigenvalue_delta = optimize_TECs(updated_surface, eigenvectors, current_eigenvalues,
                                                       eigenvalue_bounds, clear_aperture_outer, clear_aperture_inner, Z,
-                                                      metric='rms_bounded')
+                                                      metric='rms')
     eigenvalues = current_eigenvalues + eigenvalue_delta * eigen_gain
     write_eigenvalues_to_csv(tec_path + 'corrections_based_on_' + timestamp + '.csv', eigenvalues)
 
@@ -152,4 +155,5 @@ def suggest_next_iteration_of_TEC_correction(current_eigenvalues, folder_path, t
     plt.title('Surface at ' + timestamp + ' has ' + str(np.round(rms * 1000)) + 'nm wavefront error')
     plt.show()
 
-    return eigenvalues, mean_surface
+    return eigenvalues, reduced_surface
+# %%
--- test.py
+++ test.py
@@ -18,31 +18,31 @@ from LFAST_TEC_output import *
 #%%
 if __name__ == "__main__":
 
-    base_path = '/Documents/lfast/'
-    sh_path = base_path + 'on-sky/20250501/' #path pointing to folder of SH images for current night
-    reference_path = os.path.join(sh_path,'220452/') #path to set of images used for pupil definition
-    folder_path = os.path.join(sh_path, '221340/')
+    base_path = '/home/steward/lfast/'
+    sh_path = base_path + 'SHWFS/20250521/' #path pointing to folder of SH images for current night
+    reference_path = os.path.join(sh_path,'223742/') #path to set of images used for pupil definition
+    folder_path = os.path.join(sh_path, '223742/')
 
     eigenvectors_path = base_path + 'mirrors/M9/' #path to the folder containing the TEC eigenvector data from interferometer
     tec_path = eigenvectors_path
     eigenvectors = np.load(eigenvectors_path + 'eigenvectors.npy',allow_pickle=True) #load TEC eigenvector data
     eigenvalue_bounds = [] #Used for bounded optimizer (deprecated)
-    for i in np.arange(len(eigenvalues)):
-        eigenvalue_bounds.append([-0.6, 0.6])
     eigen_gain = 0.3 #Gain relating surface error to next iteration adjustement
     eigenvalues = [0]*24 #Starting values for TEC currents
- 
+    for i in np.arange(len(eigenvalues)):
+        eigenvalue_bounds.append([-0.6, 0.6])
+        
     in_to_m = 25.4e-3
     OD = 31.9 * in_to_m  # Outer mirror diameter (m)
     ID = 6 * in_to_m  # Central obscuration diameter (m)
     clear_aperture_outer = 0.47 * OD
     clear_aperture_inner = ID / 2
 
-    xyr, extend_image = xyr_pupil_definition(reference_path, sh_path)  #Define location of pupil within SH image
+    xyr, extend_image = xyr_pupil_definition(reference_path, sh_path, redefine_pupil=True)  #Define location of pupil within SH image
     jup_crop = crop_image(extend_image,xyr)
     output_plots = False
-    referenceX,referenceY,magnification,nominalSpot,rotation = lenslet_definition(reference_path, reference_path, xyr, output_plots=True)
-
+    referenceX,referenceY,magnification,nominalSpot,rotation = lenslet_definition(reference_path, sh_path, xyr, output_plots=False, recompute_rotation=True)
+#%%
     X, Y = np.meshgrid(np.arange(extend_image.shape[0]), np.arange(extend_image.shape[1]))
     proposed_pupil = np.sqrt(np.square(X - xyr[0]) + np.square(Y - xyr[1])) < xyr[2]
     cropped_pupil = crop_image(proposed_pupil, xyr)
@@ -50,10 +50,10 @@ if __name__ == "__main__":
 
     surfaces, Z = process_folder_of_images(folder_path, rotation, xyr, referenceX, referenceY, magnification,
                                            nominalSpot, xyr_cropped, output_plots=False)
-    mean_surface = np.mean(surfaces, axis=0)
+    mean_surface = np.flipud(np.fliplr(np.mean(surfaces, axis=0)))
 
     grid_diameter = (clear_aperture_outer*2)
     corresponding_pupil = make_lfast_aperture(mean_surface.shape,grid_diameter)
-
+#%%
     current_eigenvalues = eigenvalues.copy()
-    eigenvalues, surface = suggest_next_iteration_of_TEC_correction(current_eigenvalues, folder_path, tec_path, mean_surface, eigenvectors, clear_aperture_outer, clear_aperture_inner, Z, eigenvalue_bounds, eigen_gain)
+    eigenvalues, reduced_surface = suggest_next_iteration_of_TEC_correction(current_eigenvalues, folder_path, tec_path, mean_surface, eigenvectors, clear_aperture_outer, clear_aperture_inner, Z, eigenvalue_bounds, eigen_gain)
